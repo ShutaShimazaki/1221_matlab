6ef44640f81fe3882eb3e0441c766aed46ee82ce
ShutaShimazaki <tanakashimazaki0815@gmail.com>
2023-03-14 12:22:53 +0900

    save: choice2の挙動OK

--- main_template.m
+++ main_template.m
@@ -5,12 +5,17 @@
 %% ワークスペースのクリア
 clear; 
 %% 要変更！！%%%
-DATE = "230112"; %yymmdd
+DATE = "230112test"; %yymmdd
 SP = 10.5; %structual parameter (校正サンプルを測定しZENで解析して得た値を入力）
 w_radius = 0.199; %観察体積の半径を入力 （formula > calculate_w0.mlxファイルを実行すると計算してくれる）
 COMPONENT = 2; %フィットモデル式の拡散成分の数（１か２を選択。詳細は"script>fitting>fitting_temporal.mlx"を参照。）
 choice = 1; %計算したいACFの種類を選択
-% ①時間相関(デフォルト。各ピクセルの時間相関の平均。) ②時間相関(任意の１ピクセル）③時空間相関　
+% ①時間相関(デフォルト。各ピクセルの時間相関の平均。) ②時間相関(任意の１ピクセル）③時空間相関
+
+%②：任意の1ピクセルの時間相関を選択した場合は　要変更
+if choice == 2
+    constant_X = 3 ; %任意の1ピクセルconstant_Xを選択( 1 < constant_X < PIXEL数)。このピクセルだけの時間相関を計算する
+end
 %% パスを通す
 addpath(genpath("function"));
 addpath(genpath("script"));
@@ -49,8 +54,13 @@ for idx=1:length(filename_array)
     XT = double(XT);
     
     %% 光褪色による蛍光強度の減衰を補正する
-    run("correct_intensity.m")
-    XT = XT_corrected;
+    if choice == 1
+        run("correct_intensity.m")
+        XT = XT_corrected;
+    elseif choice == 2
+        run("correct_intensity_oneline.m")
+        XT = XT_corrected;
+    end
     %% 相関計算
     if choice == 1 %%各ピクセルにおける時間相関の平均を計算する
         NUMBER_TAU = 100; % tauの個数（＝上限）を設定
@@ -66,12 +76,13 @@ for idx=1:length(filename_array)
          %run("compare_with_zen.mlx")
         %Save workspace
          save(sprintf('workspace/%s/temporal_%s.mat',DATE, filename))
+        % 拡散係数などの大事なパラメータを格納した構造体を保存
+         save(sprintf('workspace/%s/important_parameters.mat',DATE), 'important_parameters_struct');
         
       elseif choice == 2 %定点X（＝1ピクセル)を選択。このピクセルだけの時間相関を計算する
-        constant_X = 3 ; 
         NUMBER_TAU = 100; % tauの個数（＝上限）を設定
         %ACF
-        [TAU, COR] = constantX_temporal_correlation(XT, TIME_SCALE, constant_X, NUMBER_TAU);
+        [TAU, COR] = constantX_temporal_correlation(XT, TIME_SCALE, NUMBER_TAU); %XTの次元:TIMESERIES×1
         %Run Fitting
          run("fitting_temporal.mlx")
         %Run Plot
@@ -82,6 +93,8 @@ for idx=1:length(filename_array)
          %run("compare_with_zen.mlx")
         %Save workspace
          save(sprintf('workspace/%s/X_temporal_%s.mat',DATE, filename))
+        % 拡散係数などの大事なパラメータを格納した構造体を保存
+         save(sprintf('workspace/%s/X_important_parameters.mat',DATE), 'important_parameters_struct');
          
     elseif choice == 3 %時空間相関
         %ACF
@@ -95,7 +108,4 @@ for idx=1:length(filename_array)
          save(sprintf('workspace/%s/spatiotemporal_%s.mat',DATE, filename))
     end
 end
-
-%% 拡散係数などの大事なパラメータを格納した構造体を保存
-save(sprintf('workspace/%s/important_parameters.mat',DATE), 'important_parameters_struct');
-disp("---- Done! ----")
+disp("---- Done !! ----")
